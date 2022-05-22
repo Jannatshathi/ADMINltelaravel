6e62d76c1866a7cf133f97c91d070fc7d8c16d93
= <=>
2022-05-22 16:55:24 +0600

    multi guard update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,25 +1,5 @@
-<!-- @extends('layouts.app')
-<a href="{{ route('admin.logout') }}" class="list-group-item list-group-item-action text-danger">Logout</a>
+@extends('layouts.app')
 @section('content')
-    <div class="col-md-9">
-        <div class="card">
-            <div class="card-header">{{ __('User Dashboard') }}</div>
-            <div class="card-body">
-                <div class="alert alert-success text-center" role="alert">
-                    WellCome To User Dashboard
-                </div>
-                @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                Hi , {{ Auth::user()->name }}
-                </br>
-                <span>User mail : {{ Auth::user()->email }}</span>
-                </br>
-                {{ __('You are logged in!') }}
-            </div>
-        </div>
-    </div>
-@endsection -->
-<h1>Jannat</h1>
+    {{ Auth::user()->name }}
+    <h1>Admin Dashboard</h1>
+@endsection
--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -1,24 +1,5 @@
 @extends('layouts.app')
-<a href="{{ route('teacher.logout') }}" class="list-group-item list-group-item-action text-danger">Logout</a>
 @section('content')
-    <div class="col-md-9">
-        <div class="card">
-            <div class="card-header">{{ __('User Dashboard') }}</div>
-            <div class="card-body">
-                <div class="alert alert-success text-center" role="alert">
-                    WellCome To User Dashboard
-                </div>
-                @if (session('status'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
-                @endif
-                Hi , {{ Auth::user()->name }}
-                </br>
-                <span>User mail : {{ Auth::user()->email }}</span>
-                </br>
-                {{ __('You are logged in!') }}
-            </div>
-        </div>
-    </div>
+    {{ Auth::user()->name }}
+    <h1>Teacher Dashboard</h1>
 @endsection
